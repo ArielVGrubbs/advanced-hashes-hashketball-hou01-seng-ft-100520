fcd6f7e23c4d35437e9f58eeea4412dfa7591fc5
Ariel Grubbs <arielvictor1234@gmail.com>
2020-10-05 21:58:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -151,7 +151,7 @@ def shoe_size(player_name)
 end
 
 def team_colors(team_name)
-  binding.pry
+
   correct_team = game_hash.find do |location, hash_info|
     hash_info[:team_name] == team_name
   end
